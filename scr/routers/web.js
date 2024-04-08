6e19d860b27e627a06bed562382412ef72ec504e
tranquoc1301 <tranquoc1301@gmail.com>
2024-04-08 11:26:28 +0700

    getJobList API

--- scr/routers/web.js
+++ scr/routers/web.js
@@ -1,5 +1,6 @@
 const { check, validationResult } = require('express-validator');
-const { getTuan, getImage, postRegisterUser, postLoginUser, test} = require('../controllers/homeController')
+const { getTuan, getImage, postRegisterUser, postLoginUser, test } = require('../controllers/homeController')
+const { getJobList, getJobListByIndustry, getJobListByArea, getJobListByName } = require('../controllers/job.Controllers')
 const express = require('express');
 const { selectFields } = require('express-validator/src/field-selection');
 
@@ -11,13 +12,21 @@ router.get('/abc', getImage)
 
 router.post('/register', [
     check('email', 'email is wrong type').isEmail(),
-    check('password', 'password is too short').isLength({ min: 8 })], 
+    check('password', 'password is too short').isLength({ min: 8 })],
     postRegisterUser)
 
 router.post('/login', [
     check('email', 'email is empty').notEmpty(),
     check('email', "email is wrong type").isEmail(),
-    check('password', 'password is too short').isLength({min: 8})], 
+    check('password', 'password is too short').isLength({ min: 8 })],
     postLoginUser)
+
+router.get('/jobs/jobList', getJobList)
+
+router.get('/jobs/jobListByIndustry', getJobListByIndustry)
+
+router.get('/jobs/jobListByArea', getJobListByArea)
+
+router.get('/jobs/jobListByName', getJobListByName)
+
 module.exports = router;
- 
